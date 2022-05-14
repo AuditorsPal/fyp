9a7760d178ab466ee3729e708402de646b77a1c6
Muhammad Waleed Asif <70438851+waleedasif10@users.noreply.github.com>
2022-05-14 19:22:52 +0500

    signout

--- lib/screens/AuditorsViews/AuditorHome.dart
+++ lib/screens/AuditorsViews/AuditorHome.dart
@@ -1,10 +1,13 @@
+import 'package:auditorpal/Colors.dart';
 import 'package:auditorpal/model/userModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:auditorpal/controlller/authenticationService.dart';
+import '../UserCheck.dart';
 
 class AuditorHome extends StatefulWidget {
-  const AuditorHome({ Key? key }) : super(key: key);
+  const AuditorHome({Key? key}) : super(key: key);
 
   @override
   State<AuditorHome> createState() => _AuditorHomeState();
@@ -13,10 +16,40 @@ class AuditorHome extends StatefulWidget {
 class _AuditorHomeState extends State<AuditorHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Auditor")),
-      body: Container(
-        child: Text(Provider.of<UserModel>(context,listen:false).userType),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "Events"),
+              Tab(text: "My Tickets"),
+            ],
+          ),
+          title: const Text("User"),
+          backgroundColor: MyColors.blueColor,
+          actions: [
+            PopupMenuButton(
+                // add icon, by default "3 dot" icon
+                // icon: Icon(Icons.book)
+                itemBuilder: (context) {
+              return [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text("Logout"),
+                ),
+              ];
+            }, onSelected: (value) {
+              if (value == 0) {
+                context.read<AuthenticationService>().signOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => userCheck()),
+                    (_) => false);
+              }
+            }),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/OrganizationViews/OrganizationHome.dart
+++ lib/screens/OrganizationViews/OrganizationHome.dart
@@ -1,10 +1,13 @@
+import 'package:auditorpal/Colors.dart';
 import 'package:auditorpal/model/userModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:auditorpal/controlller/authenticationService.dart';
+import '../UserCheck.dart';
 
 class OrganizerHome extends StatefulWidget {
-  const OrganizerHome({ Key? key }) : super(key: key);
+  const OrganizerHome({Key? key}) : super(key: key);
 
   @override
   State<OrganizerHome> createState() => _OrganizerHomeState();
@@ -13,10 +16,40 @@ class OrganizerHome extends StatefulWidget {
 class _OrganizerHomeState extends State<OrganizerHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Org")),
-      body: Container(
-        child: Text(Provider.of<UserModel>(context,listen:false).email),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: "My Events"),
+              Tab(text: "Create Events"),
+            ],
+          ),
+          title: const Text("Organizer"),
+          backgroundColor: MyColors.blueColor,
+          actions: [
+            PopupMenuButton(
+                // add icon, by default "3 dot" icon
+                // icon: Icon(Icons.book)
+                itemBuilder: (context) {
+              return [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text("Logout"),
+                ),
+              ];
+            }, onSelected: (value) {
+              if (value == 0) {
+                context.read<AuthenticationService>().signOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => userCheck()),
+                    (_) => false);
+              }
+            }),
+          ],
+        ),
       ),
     );
   }
