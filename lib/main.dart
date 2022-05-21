046ea5d3e499a0d79a8cd53caa097b962b322e2e
Muhammad Waleed Asif <70438851+waleedasif10@users.noreply.github.com>
2022-05-21 15:29:36 +0500

    registration final page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:auditorpal/screens/login.dart';
-import 'package:auditorpal/screens/signup.dart';
+import 'package:auditorpal/screens/AuditorsViews/signupaud.dart';
 import 'package:auditorpal/screens/resetpass.dart';
 import 'package:provider/provider.dart';
 import 'controlller/authenticationService.dart';
